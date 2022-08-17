41a4ed964a1a4265426d57481f6d88d5c3861e9a
dariusika23 <iddarius23@gmail.com>
2022-08-17 14:08:39 +0300

    added navbar, modified package.json to fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SignInView } from "./components/SignInView";
+import { NavigationBar } from "./components/NavigationBar";
 
 function App() {
 	return (
 		<>
-			<SignInView />
+			<NavigationBar />
+			<div className="content">
+				<SignInView />
+			</div>
 		</>
 	);
 }
